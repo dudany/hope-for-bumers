4ae0c2f82f21a5d5cc8b1598c1d3f73f43bfbcce
ddani <ddani@vmware.com>
2022-08-06 09:49:03 +0300

    prepared ingestion

--- pipelines/unemployment_etl/raw_data_ingestion/ingestion_dumper.py
+++ pipelines/unemployment_etl/raw_data_ingestion/ingestion_dumper.py
@@ -1,29 +1,66 @@
+import pandas as pd
 import requests
 import json
-import prettytable
+import os
+import logging
+from retrying import retry
+from pipelines.unemployment_etl.utils.unemployment_utils import set_logging
+from typing import Dict, List
+
+set_logging()
+
 headers = {'Content-type': 'application/json'}
-data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
+DEFAULT_SERIES_ID = ['LAUCT395688200000003', 'LAUCT394713800000003', 'LAUCT485235600000003', 'LAUCN271410000000003',
+                     'LAUCN420410000000003']
+DEFAULT_DATES = {"startyear": "2019", "endyear": "2021"}
+BUREAU_OF_LABOR_STATISTICS_API_HEADER = 'https://api.bls.gov/publicAPI/v2/timeseries/data/'
 
 
-def get_api_data():
-    p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
-    json_data = json.loads(p.text)
+@retry(stop_max_attempt_number=3, wait_fixed=2000)
+def get_data_from_api(data: str) -> str:
+    logging.info("Making API call to get labor statistics data")
+    return requests.post(BUREAU_OF_LABOR_STATISTICS_API_HEADER, data=data, headers=headers).text
+
+
+def ingest_raw_data(raw_data_location: str, date_period: Dict[str, str] = None, series_list: List[str] = None):
+    """
+
+    :param raw_data_location: String of path for the raw data to land in
+    :param date_period: Dictionary with keys `startyear` and `endyear` that representing the required date period
+    :param series_list: List of required series ids to process
+    """
+    logging.info("Starting ingestion of raw data")
+    # handles default values
+    if series_list is None:
+        series_list = DEFAULT_SERIES_ID
+    if date_period is None:
+        date_period = DEFAULT_DATES
+    header_dict = {"seriesid": series_list}
+    header_dict.update(date_period)
+    data = json.dumps(header_dict)
+
+    # making api call to ge data
+    json_data = json.loads(get_data_from_api(data))
+    # processing data from response
     for series in json_data['Results']['series']:
-        x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+        rows_list = []
         seriesId = series['seriesID']
         for item in series['data']:
-            year = item['year']
-            period = item['period']
-            value = item['value']
-            footnotes=""
+            footnotes = ""
             for footnote in item['footnotes']:
                 if footnote:
                     footnotes = footnotes + footnote['text'] + ','
-            if 'M01' <= period <= 'M12':
-                x.add_row([seriesId,year,period,value,footnotes[0:-1]])
-        output = open(seriesId + '.txt','w')
-        output.write (x.get_string())
-        output.close()
+            if 'M01' <= item['period'] <= 'M12':
+                rows_list.append(
+                    {"series_id": seriesId, "year": item['year'], "period": item['period'], "value": item['value'],
+                     "footnotes": footnotes[0:-1]})
+        series_df = pd.DataFrame(rows_list)
+        file_pattern = f"{seriesId}_{date_period['startyear']}_{date_period['endyear']}.json"
+        file_path = os.path.join(raw_data_location, file_pattern)
+        # writing to raw data location
+        series_df.to_json(file_path)
+        logging.info(f"Written file: {file_pattern}")
+
 
 if __name__ == '__main__':
-    get_api_data()
+    ingest_raw_data("/Users/ddani/Desktop/Dani/hope-for-bumers/datalake/raw_data")
---
+++ pipelines/unemployment_etl/utils/unemployment_utils.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+
+def set_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("debug.log"),
+            logging.StreamHandler()
+        ]
+    )
+
+def prepare_datalake_dirs():
+    os.mkdir('../../datalake')
